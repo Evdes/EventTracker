688b636bdcde00ea440f7215a05768c14ee88038
Evdes <Evert.De.Schryver@gmail.com>
2019-04-25 17:38:31 +0200

    Bugfixes for adding & removing timeframes

--- EventTracker/wwwroot/js/site.js
+++ EventTracker/wwwroot/js/site.js
@@ -1,19 +1,19 @@
 ﻿$(document).ready(function () {
 
     //Delete timeframes from edited event
-    $(".DeleteTimeframeButton").click(function () {
-        var timeframeToRemove = "#Timeframe_" + $(this).data('id') ;
-        $(timeframeToRemove).remove();
-    })
+    $(".DeleteTimeframeButton").click(removeTimeframe);
 
     //Add empty timeframes when adding event
     $("#addTimeframe").click(function (e) {
         e.preventDefault();
+
+        //Set counter for dynamical generation of ID attributes for modelbinding
         var i = $(".timeframes").length;
-        var dateOfToday = $("#Timeframes_0__EventDate").val();
-        var newTimeframe = `<div class="timeframes">
+
+        //Html element to be added
+        var newTimeframe = `<div class="timeframes" id="Timeframe_` + i + `">
             <label for= "Timeframes_` + i + `__EventDate" > Event Date</label >
-                <input min=` + dateOfToday + ` type="date" data-val="true" data-val-required="The Event Date field is required." id="Timeframes_` + i + `__EventDate" name="Timeframes[` + i + `].EventDate" value=` + dateOfToday + ` />
+                <input type="date" data-val="true" data-val-required="The Event Date field is required." id="Timeframes_` + i + `__EventDate" name="Timeframes[` + i + `].EventDate" />
                 <span class="field-validation-valid" data-valmsg-for="Timeframes[` + i + `].EventDate" data-valmsg-replace="true"></span>
                 <label for="Timeframes_` + i + `__Starttime">Start</label>
                 <input min="0" max="24" type="number" data-val="true" data-val-range="Invalid hour" data-val-range-max="24" data-val-range-min="0" data-val-required="The Start field is required." id="Timeframes_` + i + `__Starttime" name="Timeframes[` + i + `].Starttime" value="0" />
@@ -21,8 +21,22 @@
                 <label for="Timeframes_` + i + `__Endtime">End</label>
                 <input min="0" max="24" type="number" data-val="true" data-val-range="Invalid hour" data-val-range-max="24" data-val-range-min="0" data-val-required="The End field is required." id="Timeframes_` + i + `__Endtime" name="Timeframes[` + i + `].Endtime" value="0" />
                 <span class="field-validation-valid" data-valmsg-for="Timeframes[` + i + `].Endtime" data-valmsg-replace="true"></span>
+                <button type="button" id="DeleteTimeframeButton_` + i + `" class="DeleteTimeframeButton" data-id=` + i + `>Delete Timeframe</button>
             </div>`
-        $("#TimeframesToAdd").append(newTimeframe);
-    })
+
+        //Set click event on generated button
+        $("#TimeFramesToAdd").append(newTimeframe);
+        var button = $("#DeleteTimeframeButton_" + i);
+        button.click(removeTimeframe);
+    });
+
+    //***Helper Functions***
+    function removeTimeframe() {
+        if ($(".timeframes").length > 1) {
+            var timeframeToRemove = "#Timeframe_" + $(this).data('id');
+            $(timeframeToRemove).remove();
+        }
+    }
 });
+
 
