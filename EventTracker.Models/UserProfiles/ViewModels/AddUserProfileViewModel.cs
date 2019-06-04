a0a3dece51f7f2e71cc27d7467d8f52545da2d18
Evdes <Evert.De.Schryver@gmail.com>
2019-06-04 15:56:07 +0200

    Updated documentation

--- EventTracker.Models/UserProfiles/ViewModels/AddUserProfileViewModel.cs
+++ EventTracker.Models/UserProfiles/ViewModels/AddUserProfileViewModel.cs
@@ -19,7 +19,7 @@ namespace EventTracker.Models.UserProfiles.ViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+        [StringLength(int.MaxValue, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
